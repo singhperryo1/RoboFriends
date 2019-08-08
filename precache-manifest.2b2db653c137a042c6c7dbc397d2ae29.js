023ede55e4437fb0a5bcd1d846cbe038e0e5ea33
Singh Perry <sgagandeep232@gmail.com>
2019-08-08 15:59:55 -0700

    Updates

---
+++ precache-manifest.2b2db653c137a042c6c7dbc397d2ae29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1fb8eec7147780208c31833354f9a2",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3e5077ce4977ce62dfbc",
+    "url": "/robofriends/static/css/main.92ab38aa.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "3e5077ce4977ce62dfbc",
+    "url": "/robofriends/static/js/main.7c223821.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "bcfa3a4184aa5b37b2a4033f6ec15325",
+    "url": "/robofriends/static/media/Team 401.bcfa3a41.woff"
+  }
+]);
